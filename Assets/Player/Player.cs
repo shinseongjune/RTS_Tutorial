5cf3a6c81abd0dcb5d3e9a612ab69990f70fbbcf
sodaus145 <shinseongjune.dev@gmail.com>
2021-06-09 21:20:36 +0900

    bug fix

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         hud = GetComponentInChildren<HUD>();
+        print(startMoney);
         AddStartResourceLimits();
         AddStartResources();
     }
